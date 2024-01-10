5bb5ebe9375f9be0a9d29540873165ce1cbe4dec
warjiang <1096409085@qq.com>
2024-01-10 17:23:11 +0800

    feat: add lib/utils for tailwind

---
+++ lib/utils.ts
@@ -0,0 +1,30 @@
+import { ClassValue, clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+
+export function cn(...inputs: ClassValue[]) {
+    return twMerge(clsx(inputs));
+}
+
+
+export function nFormatter(num: number, digits?: number) {
+    if (!num) return "0";
+    const lookup = [
+        { value: 1, symbol: "" },
+        { value: 1e3, symbol: "K" },
+        { value: 1e6, symbol: "M" },
+        { value: 1e9, symbol: "G" },
+        { value: 1e12, symbol: "T" },
+        { value: 1e15, symbol: "P" },
+        { value: 1e18, symbol: "E" },
+    ];
+    const rx = /\.0+$|(\.[0-9]*[1-9])0+$/;
+    const item = lookup
+        .slice()
+        .reverse()
+        .find(function (item) {
+            return num >= item.value;
+        });
+    return item
+        ? (num / item.value).toFixed(digits || 1).replace(rx, "$1") + item.symbol
+        : "0";
+}
