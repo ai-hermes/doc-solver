51713fd3c751f5f81478d4d41d82fbc1be2264a7
warjiang <1096409085@qq.com>
2024-01-11 00:46:06 +0800

    feat: add shadcn ui components

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -1,9 +1,9 @@
 import * as React from "react"
 
-import { cn } from "@/utils/cn"
+import { cn } from "@/lib/utils"
 
 export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {}
+  extends React.InputHTMLAttributes<HTMLInputElement> { }
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, ...props }, ref) => {
