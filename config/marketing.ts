1c7587a87a96798c0c502b5b145f4cb754550575
warjiang <1096409085@qq.com>
2024-01-11 13:28:13 +0800

    feat: add validation and global config

---
+++ config/marketing.ts
@@ -0,0 +1,18 @@
+import { MarketingConfig } from "types"
+
+export const marketingConfig: MarketingConfig = {
+    mainNav: [
+        {
+            title: "Pricing",
+            href: "/pricing",
+        },
+        {
+            title: "Blog",
+            href: "/blog",
+        },
+        {
+            title: "Documentation",
+            href: "/docs",
+        },
+    ],
+}
