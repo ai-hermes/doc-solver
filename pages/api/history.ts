8528e478d10e16e8e61a0a493da43c2dd4079a51
warjiang <1096409085@qq.com>
2024-01-11 15:12:13 +0800

    feat: refactor history api

--- pages/api/history.ts
+++ pages/api/history.ts
@@ -1,28 +1,33 @@
-import { BufferMemory } from "langchain/memory";
-import { RedisChatMessageHistory } from "@langchain/community/stores/message/ioredis";
-import _ from 'lodash';
-import { prisma } from '@/utils/prisma';
-import { Message } from '@/types/chat';
-import { Document } from 'langchain/document';
 import { NextApiRequest, NextApiResponse } from "next";
+import { getServerSession } from 'next-auth';
+import { authOptions } from "./auth/[...nextauth]";
+import _ from 'lodash';
+import { Document } from 'langchain/document';
+import { getBufferMemory } from '@/lib/clients/llm';
+import { getPrismaClient } from '@/lib/clients/prisma';
 
-export async function getHistoryData() {
-    // const indexName = "Test10";
-    const uId = '123';
-    const documentId = 'd6db6fd34c5ea56fa1dc8f55df17830e';
-    const chatId = `${uId}_${documentId}`
-    const memory = new BufferMemory({
-        memoryKey: "chat_history",
-        chatHistory: new RedisChatMessageHistory({
-            sessionId: chatId, // Or some other unique identifier for the conversation
-            sessionTTL: 43200, // 1 month 30 * 24 * 60, omit this parameter to make sessions never expire
-            url: 'redis://:docsolver123456@192.168.10.6:6379/0', // Default value, override with your own instance's URL
-        })
-    });
-    // // const memoryResult = await memory.loadMemoryVariables({})
+import { Message } from '@/types/chat';
+import { Highlight } from "@/types/model";
+
+export async function getHistoryData(userId: string, documentId: string) {
+    /*
+    1. get auth info from cookie and get user info
+    2. get document id from request, check document is exist
+    3. generate chat_id
+    4. load memory from redis
+    5. extract highlight id list from message which type is ai
+    */
+    const chatId = `${userId}_${documentId}`
+    const memory = getBufferMemory(chatId);
+
     const historyMessages = await memory.chatHistory.getMessages()
-    const hsList = historyMessages.map(item => item.lc_kwargs['hs']).filter(Boolean)
-    const hsDataList = await prisma.highlights.findMany({
+    /*
+    message example:
+
+    */
+    const prisma = getPrismaClient();
+    const hsList: Array<string> = historyMessages.map(item => item.lc_kwargs['hs']).filter(Boolean)
+    const hsDataList = await prisma.highlight.findMany({
         where: {
             id: {
                 in: hsList
@@ -30,64 +35,51 @@ export async function getHistoryData() {
         }
     })
 
-    type HighlightType = typeof hsDataList[number];
-    const hsDataMap = hsDataList.reduce((p, c) => {
-        p[c.id] = c;
-        return p
-    }, {} as Record<string, HighlightType>)
+    const hsDataMap = hsDataList
+        .reduce((p, c) => {
+            p[c.id] = c;
+            return p
+        }, {} as Record<string, Highlight>)
 
-    const messages = historyMessages.map(item => {
-        // "human" | "ai" | "generic" | "system" | "function" | "tool";
-        const hs = item.lc_kwargs['hs'];
-        let hsData = null;
-        if (hsDataMap[hs]) {
-            hsData = JSON.parse(hsDataMap[hs].hs_data)
-        }
-        if (item._getType() === 'ai') {
-            return {
-                type: 'apiMessage',
-                message: item.content,
-                sourceDocs: hsData as Document[],
-            } as Message
-        } else if (item._getType() === 'human') {
-            return {
-                type: 'userMessage',
-                message: item.content,
-            } as Message
-        }
-    }).filter(Boolean) as Message[]
+    const messages = historyMessages
+        .map(item => {
+            // "human" | "ai" | "generic" | "system" | "function" | "tool";
+            const hs = item.lc_kwargs['hs'];
+            let hsData = null;
+            if (hsDataMap[hs]) {
+                hsData = JSON.parse(hsDataMap[hs].hs_data)
+            }
+            if (item._getType() === 'ai') {
+                return {
+                    type: 'apiMessage',
+                    message: item.content,
+                    sourceDocs: hsData as Document[],
+                } as Message
+            } else if (item._getType() === 'human') {
+                return {
+                    type: 'userMessage',
+                    message: item.content,
+                } as Message
+            }
+        })
+        .filter(Boolean) as Message[]
     return messages;
 }
 
 export default async function handler(
     req: NextApiRequest,
-    resp: NextApiResponse,
+    res: NextApiResponse,
 ) {
 
+    const session = await getServerSession(req, res, authOptions)
+    const userId = session?.user.id
+    if (!userId) {
+        res.status(401)
+        return
+    }
+    // [todo][dingwenjiang] extract documentId from request
+    const documentId = 'd6db6fd34c5ea56fa1dc8f55df17830e';
+    const messages = await getHistoryData(userId, documentId);
 
-    // console.log('messages', messages)
-    // console.log('memoryResult\n', z)
-
-    /*
-    const redisClient = getRedisClient()
-    const str = await redisClient.lindex(chatId, 0)
-    console.log('getRedisClient', str)
-    const data = JSON.parse(str || '{}')
-    _.set(data, 'data.hs', "1111")
-    
-    redisClient.lset(chatId, 0, JSON.stringify(data))
-    const memory = new BufferMemory({
-        memoryKey: "chat_history",
-        chatHistory: new RedisChatMessageHistory({
-            sessionId: chatId, // Or some other unique identifier for the conversation
-            sessionTTL: 43200, // 1 month 30 * 24 * 60, omit this parameter to make sessions never expire
-            url: 'redis://:docsolver123456@192.168.10.6:6379/0', // Default value, override with your own instance's URL
-        })
-    });
-    // const memoryResult = await memory.loadMemoryVariables({})
-    const z = await memory.chatHistory.getMessages()
-    console.log('memoryResult\n', z)
-    */
-    const messages = await getHistoryData()
-    resp.status(200).json(messages);
+    res.status(200).json(messages);
 }
