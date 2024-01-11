5d4d77b31ad8308c81c994068884d8724206689a
warjiang <1096409085@qq.com>
2024-01-11 13:15:15 +0800

    feat: add layout for auth and non-auth

---
+++ components/providers.tsx
@@ -0,0 +1,12 @@
+"use client"
+import React from 'react';
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { ThemeProviderProps } from "next-themes/dist/types";
+
+export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+    return (
+        <NextThemesProvider {...props}>
+            {children}
+        </NextThemesProvider>
+    );
+}
