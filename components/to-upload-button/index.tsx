09428e1d51c85366a35d6355f9c64470b4a794ca
hehuangz <1025005090@qq.com>
2024-04-18 14:19:23 +0800

    feat: update document list

--- components/to-upload-button/index.tsx
+++ components/to-upload-button/index.tsx
@@ -1,15 +1,29 @@
 import { useRouter } from "next/router"
 import { Icons } from "../shared/icons"
 import { Button } from "@/components/ui/button"
+import { useSession } from 'next-auth/react';
+import { toast } from "../ui/use-toast";
 
 interface ToUploadButtonProps {}
 
 export function ToUploadButton({ }: ToUploadButtonProps) {
     const router = useRouter()
+    const { data: session } = useSession()
+
+    const onClick = () => {
+        if(!session?.user) {
+            return toast({
+                title: "Permission Error.",
+                description: "login first please ~",
+                variant: "destructive",
+            })
+        }
+        router.push(`/embedding`)
+    }
 
     return (
         <div className="flex justify-center items-center p-3">
-            <Button variant={'ghost'} onClick={() => router.push(`/embedding`)}>
+            <Button variant={'ghost'} onClick={onClick}>
                 <Icons.upload className="w-5 mr-1" />
                 Add Files
             </Button>
